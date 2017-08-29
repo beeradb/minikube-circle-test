9fcdfd47d264fde41403e39b0e6d1da03e3088a0
Brad Bowman <beeradb@gmail.com>
2017-08-29 16:50:38 -0600

    [master] add kubernetes is ready output

--- .circleci/minikube_start.sh
+++ .circleci/minikube_start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -o errexit
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
@@ -15,6 +14,7 @@ for i in {1..150} # timeout for 5 minutes
 do
    ./kubectl get po &> /dev/null
    if [ $? -ne 1 ]; then
+      echo "kubernetes is ready"
       break
   fi
   sleep 2
